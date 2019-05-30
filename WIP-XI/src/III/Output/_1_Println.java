cc53e8422b310496a104053d051db45741764e0a
intouchy482 <intouch482@gmail.com>
2019-05-30 11:37:30 +0700

    [ADD} new project
    วันนี้เปลี่ยนสีไปเเล้ว มีความสุขจังเลย

---
+++ WIP-XI/src/III/Output/_1_Println.java
@@ -0,0 +1,18 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package III.Output;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class _1_Println {
+    public static void main(String[] args) {
+        String name = "บาส เท่ปะหล่ะ";
+        System.out.println("หวัดดีจั๊บ ผมชื่อ " + name);
+        
+        System.out.println("ถ้าเราเอา " + 50000 + " ไปบวก " + 4900 + " เราจะได้ " + 54+900);
+    }
+}
---
+++ WIP-XI/src/IV/Operators/Operator.java
@@ -0,0 +1,31 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package IV.Operators;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class Operator {
+    public static void main(String[] args) {
+        int x = 55;
+        int y = 674321;
+        int z = 4682
+        
+        System.out.println("+ : "+ (x + y) );
+        
+        System.out.println("- : "+ (x - y) );
+        
+        System.out.println("* : "+ (x * y) );
+        
+        System.out.println("/ : "+ (y / x) );
+        
+        System.out.println("-y : "+ (x - -y) );
+        
+        System.out.println("% : "+ (y % x) );
+        
+        System.out.printf("- : "+  (x + z));
+    }
+}
